ab69c7cca4c89ca6409041a7f6c0d140c8b910bc
CloudY9 (Danya) <daniil.rakhimkulov@mail.ru>
2024-11-15 14:15:26 +0300

    Ad2122

--- calculate.py
+++ calculate.py
@@ -45,9 +45,7 @@ if __name__ == "__main__":
         func = input(f"Enter function name, avaliable are {funcs}:\n")
 
     while len(size) != sizes.get(f"{func}-{fig}", 1):
-        size = list(map(int, input("Input figure sizes separated by space, 1 for circle and square\n").split(' ')))
+        size = list(map(int, input(
+            "Input figure sizes separated by space, 1 for circle and square\n").split(' ')))
 
     calc(fig, func, size)
-
-
-
